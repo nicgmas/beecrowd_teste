f62bcaa5e90eee012d9951d97f2838dd6be9408a
nicgmas <guimaraes.nicolly2007@gmail.com>
2026-01-28 12:04:18 -0300

    Add files via upload

---
+++ soma.js
@@ -0,0 +1,7 @@
+let numA,numB,SOMA;
+numA = Number(prompt("Digite um número inteiro:"));
+numB = Number(prompt("Digite um outro número inteiro:"));
+
+SOMA = numA + numB;
+alert("SOMA = " + SOMA);
+document.writeln("SOMA =" + SOMA);
